622f916dd31ab67a862bca1dc1cf1772696d3e78
halfgrayhuman <138070452+halfgrayhuman@users.noreply.github.com>
2025-05-20 09:05:17 -0400

    Gigachad

--- src/components/backgrounds/theme/theme_backgrounds/ThemeBackground.jsx
+++ src/components/backgrounds/theme/theme_backgrounds/ThemeBackground.jsx
@@ -3,8 +3,11 @@ import { useSettings} from "../../../../hooks/useSettings";
 
 import { Box } from "@chakra-ui/react";
 
+import useMedia from "../../../../hooks/useMedia";
+
 const ThemeBackground = () => {
     const { settings } = useSettings();
+    const mobile = useMedia(['(min-width: 750px)', '(max-width: 750px)'], [false, true])
     useEffect(() => {
         console.log(settings?.seniorPrank);
     }
@@ -14,7 +17,7 @@ const ThemeBackground = () => {
         <Box 
             height="100vh" 
             width="100vw" 
-            bgImg={settings?.seniorPrank ? "https://storage.ko-fi.com/cdn/useruploads/display/d8ae04ae-c228-4284-860d-243b4c954c56_39wallpaper.png" : null} 
+            bgImg={mobile ? settings?.seniorPrank ? "https://i.pinimg.com/originals/28/27/77/2827771068d374a49d2c0fa49d30967d.gif" : null : settings?.seniorPrank ? "https://storage.ko-fi.com/cdn/useruploads/display/d8ae04ae-c228-4284-860d-243b4c954c56_39wallpaper.png" : null} 
             backgroundSize="cover"
             onError={(e) => {
                 e.target.style.backgroundImage = 'none';
