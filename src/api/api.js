32c68e1b80619482bd67bfc84f0111c4c23fae01
legojrp <legojrp@gmail.com>
2025-08-12 00:39:32 -0400

    update env to make it work

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = process.env.API_URL;
+const url = process.env.REACT_APP_API_URL;
 
 // const url = "https://schedule-client-api.herokuapp.com";
 // const url = "http://localhost:5000"
